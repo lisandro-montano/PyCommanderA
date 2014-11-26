e5867c7499bf5e1b275984182a50017a6cba724a
lisandro-montano <lisandro.montano@jalasoft.com>
2014-11-26 12:54:42 -0400

    Create Action Bar and updates on UI components

--- panel_manager.py
+++ panel_manager.py
@@ -4,13 +4,17 @@ from PyQt4 import QtCore
 
 from panel_view import PanelView
 
-class PanelManager(QtGui.QSplitter):
+class PanelManager(QtGui.QDockWidget):
 
 	def __init__(self):
 		"""Initialize the ListView panels for PyCommanderA"""
 		super(PanelManager, self).__init__()
+		panel_splitter = QtGui.QSplitter()
 		left_panel = PanelView()
 		right_panel = PanelView()
 
-		self.addWidget(left_panel)
-		self.addWidget(right_panel)
+		panel_splitter.addWidget(left_panel)
+		panel_splitter.addWidget(right_panel)
+
+		self.setWidget(panel_splitter)
+		self.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
