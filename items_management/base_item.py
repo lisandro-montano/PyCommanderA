a773ca5dffd05341cd284f05fec7d001a252bf7d
lisandro-montano <lisandro.montano@jalasoft.com>
2014-12-02 16:11:28 -0400

    Doc Updates for BaseItem

--- items_management/base_item.py
+++ items_management/base_item.py
@@ -4,6 +4,6 @@ from PyQt4 import QtCore
 
 class BaseItem(QtGui.QFileSystemModel):
 	def __init__(self, current_path):
-		"""Create Base Item based on """
+		"""Create BaseItem to handle all item_operations"""
 		super(BaseItem, self).__init__()
 		self.setRootPath(current_path)
