ed031069f3ae109868eee22aa72e042a9622ad9e
lisandro-montano <lisandro.montano@jalasoft.com>
2014-12-05 09:56:00 -0400

    Fixed indentation problems

--- views/panels/panel_operations.py
+++ views/panels/panel_operations.py
@@ -20,11 +20,11 @@ class PanelOperations(object):
 				shutil.copytree(item_path, target_path + item_name, symlinks = False, ignore = None)
 
 	def move_items(self, origin_paths, target_path):
-    	"""Move items from origin_paths to target_path
+		"""Move items from origin_paths to target_path
 
-    	Params:
-    	- origin_paths: List of item paths, names and item type to determine the method for moving it
-    	- target_path: Path where all items from origint_paths will be copied to e.g "c:\"
-    	"""
-    	for item_path, item_name, item_type in origin_paths:
-    		shutil.move(item_path, target_path)
+		Params:
+		- origin_paths: List of item paths, names and item type to determine the method for moving it
+		- target_path: Path where all items from origint_paths will be copied to e.g "c:\"
+		"""
+		for item_path, item_name, item_type in origin_paths:
+			shutil.move(item_path, target_path)
