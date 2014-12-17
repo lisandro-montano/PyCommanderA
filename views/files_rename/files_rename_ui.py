ed59f1609872d2eb262737b376492f686303ac51
gabriela-becerra-villanueva <gabinlove@gmail.com>
2014-12-17 07:35:44 -0400

    Added the ability to insert the indexes list of the selected files

--- views/files_rename/files_rename_ui.py
+++ views/files_rename/files_rename_ui.py
@@ -2,27 +2,36 @@ import sys
 
 from PyQt4 import QtGui
 from PyQt4.QtCore import SIGNAL
-from PyQt4.QtGui import QVBoxLayout, QWidget, QPushButton, QTextEdit, QDockWidget
+from PyQt4.QtGui import QVBoxLayout, QWidget, QPushButton, QTableView, QDockWidget, QLineEdit, QStandardItemModel
 
 
 class FilesRenameUI(QDockWidget):
-    def __init__(self):
+    def __init__(self, indexes_list):
         super(FilesRenameUI, self).__init__()
 
         self.setGeometry(200, 100, 400, 300)
         self.setWindowTitle('Rename multiple files')
         self.panel_splitter = QtGui.QSplitter()
 
+        self.names = QLineEdit()
+        self.names.setObjectName("Name")
+        self.names.setText("[N]")
+
+        self.extension = QLineEdit()
+        self.extension.setObjectName("Extension")
+        self.extension.setText("[E]")
+
         self.current_names_label = QtGui.QLabel("Current items name")
         self.new_names_label = QtGui.QLabel("New items name")
 
-        self.current_names = QTextEdit()
-        self.current_names.setObjectName("Current selected items name")
-        self.current_names.setText("Current names")
+        self.model1 = QStandardItemModel(self)
+        self.current_names = QTableView()
+        self.current_names.setModel(self.model1)
 
-        self.new_names = QTextEdit()
-        self.new_names.setObjectName("New selected items name")
-        self.new_names.setText("New names")
+        self.model2 = QStandardItemModel(self)
+        self.new_names = QTableView()
+        self.new_names.setModel(self.model2)
+        self.new_names.append_selected_files(indexes_list)
 
         self.cancel_button = QPushButton()
         self.cancel_button.setObjectName("cancel")
@@ -34,12 +43,14 @@ class FilesRenameUI(QDockWidget):
 
         self.panel_1 = QWidget()
         self.panel_h_layout1 = QVBoxLayout()
+        self.panel_h_layout1.addWidget(self.names)
         self.panel_h_layout1.addWidget(self.current_names_label)
         self.panel_h_layout1.addWidget(self.current_names)
         self.panel_h_layout1.addWidget(self.cancel_button)
 
         self.panel_2 = QWidget()
         self.panel_h_layout2 = QVBoxLayout()
+        self.panel_h_layout2.addWidget(self.extension)
         self.panel_h_layout2.addWidget(self.new_names_label)
         self.panel_h_layout2.addWidget(self.new_names)
         self.panel_h_layout2.addWidget(self.rename_button)
@@ -53,6 +64,10 @@ class FilesRenameUI(QDockWidget):
         self.setWidget(self.panel_splitter)
 
         self.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
+
+    def append_selected_files(self, indexes_list):
+        for index in indexes_list:
+            self.new_names.model().appendRow(index)
 
 app = QtGui.QApplication(sys.argv)
 form = FilesRenameUI()
